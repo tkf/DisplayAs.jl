7ed0df59a5c3b152e14b6eb7ac1a4212d96cc677
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-08 18:17:08 -0700

    Add mime_str macro

--- src/DisplayAs.jl
+++ src/DisplayAs.jl
@@ -10,6 +10,21 @@ Showable{mime}(content) where {mime <: MIME} = Showable(mime(), content)
 Base.show(io::IO, ::mime, s::Showable{mime}) where mime <: MIME =
     show(io, mime(), s.content)
 
+"""
+    mime"..." :: Type{<:Showable}
+
+# Examples
+```jldoctest
+julia> using DisplayAs
+
+julia> DisplayAs.mime"text/plain" === DisplayAs.Text
+true
+```
+"""
+macro mime_str(s)
+    :(Showable{MIME{Symbol($s)}})
+end
+
 const Text = Showable{MIME"text/plain"}
 const MD = Showable{MIME"text/markdown"}
 const HTML = Showable{MIME"text/html"}
