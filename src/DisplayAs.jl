aa1974bda2b8adfc7eecb6879f0c2faa8b607454
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-08 17:21:59 -0700

    Add Showable wrapper

--- src/DisplayAs.jl
+++ src/DisplayAs.jl
@@ -1,5 +1,25 @@
 module DisplayAs
 
-greet() = print("Hello World!")
+struct Showable{MT <: MIME}
+    mime::MT
+    content
+end
+
+Showable{mime}(content) where {mime <: MIME} = Showable(mime(), content)
+
+Base.show(io::IO, ::mime, s::Showable{mime}) where mime <: MIME =
+    show(io, mime(), s.content)
+
+const Text = Showable{MIME"text/plain"}
+const MD = Showable{MIME"text/markdown"}
+const HTML = Showable{MIME"text/html"}
+const JSON = Showable{MIME"application/json"}
+const SVG = Showable{MIME"image/svg+xml"}
+const PNG = Showable{MIME"image/png"}
+const PDF = Showable{MIME"application/pdf"}
+const EPS = Showable{MIME"application/eps"}
+const JPEG = Showable{MIME"image/jpeg"}
+const PS = Showable{MIME"application/postscript"}
+const LaTeX = Showable{MIME"text/latex"}
 
 end # module
